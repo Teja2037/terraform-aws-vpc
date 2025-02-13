1db1ac949819841ace3a0d9e92b3ba7057d9c636
Teja2037 <mall.varunteja@gmail.com>
2025-02-13 21:06:13 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -6,83 +6,22 @@ variable "environment" {
 
 }
 
-variable "vpc_cidr" {
+variable "sg_name" {
 
 }
 
-variable "enable_dns_hostname" {
-    default = true
+variable "sg_description" {
+
+}
+
+variable "vpc_id" {
+
 }
 
 variable "common_tags" {
     type = map
-    #default = {}
 }
 
-variable "vpc_tags" {
-    default = {}
-}
-
-variable "igw_tags" {
-    default = {}
-}
-
-variable "public_subnet_cidrs" {
-    type = list
-    validation {
-        condition     = length(var.public_subnet_cidrs) == 2
-        error_message = "please provide 2 valid public subnet CIDR"
-    }
-}
-
-variable "public_subnet_tags" {
-    default = {}
-}
-
-variable "private_subnet_cidrs" {
-    type = list
-    validation {
-        condition     = length(var.private_subnet_cidrs) == 2
-        error_message = "please provide 2 valid private subnet CIDR"
-    }
-}
-
-variable "private_subnet_tags" {
-    default = {}
-}
-
-variable "database_subnet_cidrs" {
-    type = list
-    validation {
-        condition     = length(var.database_subnet_cidrs) == 2
-        error_message = "please provide 2 valid database subnet CIDR"
-    }
-}
-
-variable "database_subnet_tags" {
-    default = {}
-}
-
-variable "nat_gateway_tags" {
-    default = {}
-}
-
-variable "public_route_table_tags" {
-    default = {}
-}
-
-variable "private_route_table_tags" {
-    default ={}
-}
-
-variable "database_route_table_tags" {
-    default = {}
-}
-
-variable "is_peering_required" {
-    default = false
-}
-
-variable "aws_vpc_peering_tags" {
+variable "sg_tags" {
     default = {}
 }
